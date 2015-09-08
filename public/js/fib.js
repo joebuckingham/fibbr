4155261709524df0712f0712e81d553b84bbb13d
Joe Buckingham <joe_buckingham@live.co.uk>
2015-09-08 17:30:15 +0100

    Modularisation
    Moved all JS code into fib.js

--- public/js/fib.js
+++ public/js/fib.js
@@ -1,3 +1,14 @@
+var notifications = 0;
+var focused = true;
+var socket = io();
+function notifUpdate(add) {
+  notifications += add;
+  if (notifications > 0 && !focused) {
+    document.title = "(" + notifications + ") fibbr";
+  } else {
+    document.title = "fibbr";
+  }
+}
 $(document).ready(function() {
   $("#div_create").hide();
   $("#div_join").hide();
@@ -9,5 +20,30 @@ $(document).ready(function() {
   $("#btn_join").click(function() {
     $("#div_intro").slideUp();
     $("#div_join").slideDown();
+  });
+  $(window).focus(function(){
+    focused = true;
+    notifications = 0;
+    notifUpdate(0);
+  })
+  $(window).blur(function() {
+    focused = false;
+  })
+  $('#chatform').submit(function(){
+    socket.emit('chat_msg', $('#chatname').val(), $('#chatinput').val());
+    $('#chatinput').val('');
+    return false;
+  });
+  socket.on('chat_msg', function(usr, msg){
+    notifUpdate(1);
+    $('#messages').append($('<p>').html("<strong>" + usr + ":</strong> " + msg));
+    $('#messages').animate({
+      scrollTop: $('#messages').get(0).scrollHeight}, 2000);
+  });
+  socket.on('chat_alert', function(icn, msg){
+    notifUpdate(1);
+    $('#messages').append($('<p>').html("<span class='glyphicon glyphicon-" + icn + "'></span>&nbsp;" + msg));
+    $('#messages').animate({
+      scrollTop: $('#messages').get(0).scrollHeight}, 2000);
   });
 });
